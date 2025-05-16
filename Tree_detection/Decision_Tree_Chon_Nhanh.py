c7bd39f465b18337a2313621e492d19e2c366cbe
Bao <universitynhb@gmail.com>
2025-05-16 12:13:47 +0700

    giai quyet xung dot Tree_detection/Decision_Tree_Chon_Nhanh.py

--- Tree_detection/Decision_Tree_Chon_Nhanh.py
+++ Tree_detection/Decision_Tree_Chon_Nhanh.py
@@ -160,14 +160,7 @@ try:
     if not all(col in data[0] for col in expected_columns):
         missing_cols = [col for col in expected_columns if col not in data[0]]
         raise KeyError(f"Các cột không khớp. Thiếu cột: {missing_cols}")
-<<<<<<< HEAD
-    
-    print("\nBắt đầu xây dựng cây quyết định (chỉ nhánh age > 40 với nút gốc)...")
-    # Build tree with root node, but only for age > 40 branch
-    decision_tree = build_decision_tree(data, attributes, target_col, filter_attr="age", filter_value="<=30")
-=======
     decision_tree = build_decision_tree(data, attributes, target_col, filter_attr="student", filter_value="no")
->>>>>>> a60df7f5d8de72be23594ca5ddbf35a385bfd1c1
     
     print("\nVẽ cây bằng Graphviz...")
     dot = draw_tree(decision_tree)
